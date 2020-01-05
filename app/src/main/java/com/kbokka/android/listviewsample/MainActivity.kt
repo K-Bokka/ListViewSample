f5a9cf43694fa29736e4dacf0517a11844b1dd49
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2020-01-05 13:03:51 +0900

    Add listener

--- app/src/main/java/com/kbokka/android/listviewsample/MainActivity.kt
+++ app/src/main/java/com/kbokka/android/listviewsample/MainActivity.kt
@@ -2,11 +2,24 @@ package com.kbokka.android.listviewsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Adapter
+import android.widget.AdapterView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+
+  private inner class ListItemClickListener : AdapterView.OnItemClickListener {
+    override fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+      val item = parent.getItemAtPosition(position) as String
+      val show = getString(R.string.ts_word) + item
+
+      Toast.makeText(applicationContext, show, Toast.LENGTH_LONG).show()
     }
+  }
 }
