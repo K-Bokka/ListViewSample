1e716814a2bcb70cea4c15c273871f7cd9eada7f
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2020-01-05 13:12:17 +0900

    Add toast

--- app/src/main/java/com/kbokka/android/listviewsample/MainActivity.kt
+++ app/src/main/java/com/kbokka/android/listviewsample/MainActivity.kt
@@ -1,17 +1,20 @@
 package com.kbokka.android.listviewsample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Adapter
 import android.widget.AdapterView
+import android.widget.ListView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    val lvMenu = findViewById<ListView>(R.id.lvMenu)
+    lvMenu.onItemClickListener = ListItemClickListener()
   }
 
   private inner class ListItemClickListener : AdapterView.OnItemClickListener {
